29a274f4e2f3093cbb0ea92db6abe922c45a2621
ivan <ivan-guita@hotmail.com>
2024-09-13 21:31:39 -0300

    basic explanation of jsx and creation of a second example component, end of class 3

---
+++ pages/basicos/segundo.js
@@ -0,0 +1,14 @@
+/*
+JSX
+
+É uma tecnicologia capaz de fundir html e jsvascript com o objetiovo de facilitar a criação de componentes
+e diminuir a quantidade de linhas de côdigo. No caso o navegado ele não entende jsx de forma nativa então nesse 
+caso o react possui recursos qua fazem a tradução para que navegador possa entender essa sintaxe.Básicamente ele 
+recebe o jsx e transforma em js puro para que o navegador possa entender e reproduzir 
+
+
+*/
+
+export default function segundo(){
+     return<h1>Segundo</h1>
+}
