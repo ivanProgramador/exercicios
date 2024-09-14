7f20001efb1a28cf46b31a4bab5bddbf753b2c95
ivan <ivan-guita@hotmail.com>
2024-09-14 18:50:58 -0300

    rendering an external component

---
+++ Compontents/Titulo.jsx
@@ -0,0 +1,7 @@
+export default function Titulo(){
+
+    return <>
+             <h1>Titulo</h1>
+             <h2>Subtitulo</h2>
+           </>
+}
---
+++ pages/basicos/titulo.jsx
@@ -0,0 +1,12 @@
+import Titulo from "../../Compontents/Titulo";
+
+/*
+Renderizando um componente externo
+*/
+
+export default function titulo(){
+
+    return <>
+             <Titulo></Titulo>
+           </>
+}
