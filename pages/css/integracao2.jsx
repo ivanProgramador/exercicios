725b49c864a4654d1bc0416c77d5d4f92b97b2aa
ivan <ivan-guita@hotmail.com>
2024-09-15 23:11:24 -0300

    Integration of CSS modules specifically and globally for a component

---
+++ pages/css/integracao2.jsx
@@ -0,0 +1,12 @@
+import estilos from "./integracao2.module.css";
+
+
+export default function integracao2(){
+    return(
+        <div id={estilos.integracao2}>
+         <h1 className={estilos.vermelha2} >texto#01</h1>
+         <h1 className={estilos.azul2}>texto#02</h1>
+         <h1 className={estilos.branca2} >texto#03</h1>
+        </div>
+    );
+}
