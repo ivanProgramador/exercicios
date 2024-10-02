471402a682a382b9c90dded84be52ff9481f3a9f
ivan <ivan-guita@hotmail.com>
2024-10-02 16:06:21 -0300

    Rendering data coming from an api through a component

---
+++ pages/api/metodo.js
@@ -0,0 +1,15 @@
+
+/*
+Essa função anônima mostra como é possivel identificar o tipo de rerquisições recebidas   
+
+*/
+
+export default (req,res)=>{
+     
+    if(req.method === "GET"){
+        res.status(200).json({nome:"Pedro"})
+    }else{
+        res.status(200).json({nome:"Maria"})
+
+    }
+}
---
+++ pages/questao.jsx
@@ -0,0 +1,79 @@
+import { useState } from "react"
+import { useEffect } from "react"
+
+/*
+Nesse compneete eu eestou consultando uma api
+para que eu consiga mostrar os dados vindos dela em 
+um componente esses dados precisam ser tratados.
+
+antes de fazer uma reuisição primeiro euu preciso definir onde eu vou 
+colocar esses dados quando eles chegarem para que possam ser exbibidos 
+no meu componente para isso eu uso o useState() que recebe um array de 
+2 posições, na primeira posição eu informeo avarivel que receberá os 
+valores e na segunda eu informo o nomeda faunção que será usada para 
+altera-la.
+
+ const [questao, setQuestao] = useState(null)
+
+Agora eu preciso de fato pegar os dados no caso eu ciono a função useEffect()
+essa função recebe 2 parâmetros 
+
+1 - e a função que vai de fato executar a requisição e tratar a resposta
+2 - é um array de dependencias/ como não tem então eu deixei vazio(mas tem que ter esse array)  
+
+a função que vai requisitar usa 3 processos sincronos para tratar os dados 
+
+1 - fetch -> essa função requisita os dados da url 
+2 - então converte para json - > then(resp => resp.json()) 
+3 - então aciona a função que latera o estado passando o json como parametro -> then(setQuestao) 
+
+agora o estado do componente recebe um json com os dados 
+
+
+*/
+
+export default function questao (){
+
+    
+    const [questao, setQuestao] = useState(null)
+
+    useEffect(()=>{
+        fetch('http://localhost:3000/api/questao/123')
+         .then(resp => resp.json())
+         .then(setQuestao)
+    },[]);
+
+
+
+    function renderizarRespostas(){
+        if(questao){
+
+            // como o atributo repostas é um array eu tenho que usar o map para fazer o loop em todas as opções
+            // e para cada opções que ele achar ele vai gerar uma chave para o indice e um elemento <li> para lista
+
+            return questao.respostas.map((resp,i)=>{
+                return <li key={i} >{resp}</li>
+            })
+        }
+        return false 
+    }
+
+
+    
+    
+    //baixo eu mostro o enunciado da questão usando o simbolo ? ue serve para testar se esse atributo esta vázio
+    // e logo abaixo na lista eu mostro o retorno da função que seria uma lista com opções
+
+    return(
+        <div>
+          <h1>questão</h1>
+          <div>
+             <span>{questao?.enunciado}</span>
+             <ul>
+                 {renderizarRespostas()}
+             </ul>
+          </div>
+
+        </div>
+    )
+}
