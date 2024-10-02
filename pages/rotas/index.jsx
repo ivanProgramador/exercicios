687e70ed25bcae06cb94a2d1024c2fadd1109ab9
ivan <ivan-guita@hotmail.com>
2024-10-02 10:38:02 -0300

    finished route classes

--- pages/rotas/index.jsx
+++ pages/rotas/index.jsx
@@ -1,20 +1,26 @@
 import Link from "next/link"
+import router,{ useRouter} from "next/router"
 
 /*
-No caso para navegção entre páginas o Next possui o componente Link 
+No caso para navegação entre páginas o Next possui o componente Link 
 esse componente recebe como parametro o href que contem a url que será 
 acionada e dessa forma qualquer elemento html sej ele paragrafo,div,imagem
-que estiver dentro desse componete automáticamente se torna um link 
+que estiver dentro desse componente automáticamente se torna um link 
 para a rota 
-que está descrita no parametro href.   
+que está descrita no parâmetro href.   
 
 */
 
-
 export default function rotas(){
+
+   
+  
+   
+
     return(
         <div>
             <h1>Rotas index</h1>
+
             <ul>
                 <Link href="/rotas/params?id=123&nome=maria" >
                    <li>Params</li>
@@ -22,8 +28,19 @@ export default function rotas(){
                 <Link href="/rotas/123/buscar">
                    <li>Buscar</li>
                 </Link>
-
             </ul>
+
+            <div style={{display:"flex",flexDirection:"column",alignItems:"flex-start"}} >
+
+                <button onClick={()=>router.push("/rotas/123/buscar")}>Buscar</button>
+                <button onClick={()=> router.push({
+                    pathname:"rotas/params",query:{
+                        id:"123",
+                        nome:"Ana"
+                    }
+                })}>Params</button>
+
+            </div>
         </div>
     )
 }
